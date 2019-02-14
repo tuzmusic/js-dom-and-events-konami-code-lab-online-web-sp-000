f28d023e7d68521aceae3b1fc087c46fd7fcf4f5
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-14 11:53:52 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,30 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let array = []
 
+  document.body.addEventListener('keydown', (e) => {
+    let index = array.length
+    let num = parseInt(e.which)
+    if (num === code[index]) {
+      array.push(num)
+    } else {
+      array = []
+    }
+    console.log(array, num);
+    if (array.equals(code)) {
+      alert('Unlimited lives unlocked!')
+    }
+  })
 }
+
+Array.prototype.equals = function (array) {
+  if (!(array instanceof Array) || this.length !== array.length) { return false }
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== this[i]) { return false }
+  }
+  return true
+}
+
+init()
